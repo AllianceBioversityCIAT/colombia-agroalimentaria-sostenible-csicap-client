6d2d88337c01061825883ae9e33eae6f9134828a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-03-03 15:07:32 -0500

    :sparkles: feat: components added

---
+++ client/src/app/pages/platform/page/components-and-axes/components-and-axes.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponentsAndAxesComponent } from './components-and-axes.component';
+
+describe('ComponentsAndAxesComponent', () => {
+  let component: ComponentsAndAxesComponent;
+  let fixture: ComponentFixture<ComponentsAndAxesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ComponentsAndAxesComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ComponentsAndAxesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/pages/platform/page/components-and-axes/components-and-axes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components-and-axes',
+  imports: [],
+  templateUrl: './components-and-axes.component.html',
+  styleUrl: './components-and-axes.component.scss'
+})
+export class ComponentsAndAxesComponent {
+
+}
