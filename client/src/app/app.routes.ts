0cfafda19af1c5908e8be0c9654c25f95efc077d
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-03-03 15:19:29 -0500

    :sparkles: feat: platform routing added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/platform/platform.component')
+    loadComponent: () => import('./pages/platform/platform.component'),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/platform/page/home/home.component'),
+      }
+    ]
   }
 ];
--- client/src/app/pages/platform/platform.component.ts
+++ client/src/app/pages/platform/platform.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import SidebarComponent from '../../components/sidebar/sidebar.component';
+import ToolbarComponent from '../../components/toolbar/toolbar.component';
 @Component({
   selector: 'app-platform',
-  imports: [],
+  imports: [RouterOutlet, SidebarComponent, ToolbarComponent],
   templateUrl: './platform.component.html'
 })
  export default class PlatformComponent {
