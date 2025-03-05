d18cfa959b09925165211448f8fb722dba091e46
Julian Sanchez <julian.sanchez@cgiar.org>
2025-03-05 15:49:00 -0500

    ✨ feat: sidebar and toolbar developed

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [OverlayBadgeModule],
   templateUrl: './sidebar.component.html'
 })
 export default class SidebarComponent {
 
+  menuItems = signal([
+    { icon: 'pi-home', label: 'Menu principal' },
+    { icon: 'pi-sitemap', label: 'Arquitectura' },
+    { icon: 'pi-chart-line', label: 'Planes operativos' },
+    { icon: 'pi-user-edit', label: 'Gestión de usuarios' },
+    { icon: 'pi-question-circle', label: 'Acerca de roles' },
+    { icon: 'pi-calendar', label: 'Fechas clave' }
+  ]);
+
+  accountItems = signal([
+    { icon: 'pi-cog', label: 'Ajustes' },
+    { icon: 'pi-sign-out', label: 'Cerrar sesión' }
+  ]);
 }
--- client/src/app/components/toolbar/toolbar.component.ts
+++ client/src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteCompleteEvent, AutoCompleteModule } from 'primeng/autocomplete';
+import { AvatarModule } from 'primeng/avatar';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [FormsModule, AutoCompleteModule, AvatarModule, OverlayBadgeModule, InputGroupModule, InputGroupAddonModule, InputTextModule, SelectModule, InputNumberModule],
   templateUrl: './toolbar.component.html'
 })
 export default class ToolbarComponent {
+  items: any[] | undefined;
+  selectedItem: any;
+  suggestions: any[] | undefined;
+  text1: string | undefined;
+  search(event: AutoCompleteCompleteEvent) {
+      this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+  }
 
 }
