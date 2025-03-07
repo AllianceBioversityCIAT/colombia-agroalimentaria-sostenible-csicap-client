9850caa2cfb4119cb52bf4344a7f77da5886f812
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-03-07 08:40:21 -0500

    :white_check_mark: test: test fixed

--- client/src/app/pages/landing/landing.component.spec.ts
+++ client/src/app/pages/landing/landing.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-import  LandingComponent  from './landing.component';
+import LandingComponent from './landing.component';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,9 +10,25 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingComponent]
-    })
-    .compileComponents();
+      imports: [LandingComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            queryParams: of({}),
+            snapshot: {
+              paramMap: {
+                get: () => null
+              },
+              queryParamMap: {
+                get: () => null
+              }
+            }
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
