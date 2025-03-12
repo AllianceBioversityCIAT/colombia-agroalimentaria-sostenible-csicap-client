82ce1c8d64da7cc178f0d9e54bef8eb680c60fe5
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-03-12 10:57:41 -0500

    :sparkles: feat: login endpoint added

--- client/src/app/shared/services/cache/cache.service.ts
+++ client/src/app/shared/services/cache/cache.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ToPromiseService } from '../to-promise.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CacheService {}
+export class CacheService {
+  TP = inject(ToPromiseService);
+
+  login = (awsToken: string): Promise<MainResponse<LoginRes>> => {
+    const url = () => `authorization/login`;
+    return this.TP.post(url(), {}, { token: awsToken, isAuth: true });
+  };
+}
