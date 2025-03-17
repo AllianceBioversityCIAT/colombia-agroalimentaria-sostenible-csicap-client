3ff9357cc1beef565e78497a0125dbfa390efa90
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-03-17 14:59:49 -0500

    :white_check_mark: test: test added

--- client/src/app/shared/components/sidebar/sidebar.component.spec.ts
+++ client/src/app/shared/components/sidebar/sidebar.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import  SidebarComponent  from './sidebar.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import SidebarComponent from './sidebar.component';
+import { ActionsService } from '../../services/actions.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let mockActionsService: Partial<ActionsService>;
 
   beforeEach(async () => {
+    mockActionsService = {
+      logOut: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
-    })
-    .compileComponents();
+      imports: [SidebarComponent, RouterTestingModule],
+      providers: [{ provide: ActionsService, useValue: mockActionsService }],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
