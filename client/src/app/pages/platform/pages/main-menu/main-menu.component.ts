9820a9fa0e5db90212c348444a5d3ac1b8c3a733
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-04-08 09:29:35 -0500

    :sparkles: feat: redirect added

--- client/src/app/pages/platform/pages/main-menu/main-menu.component.ts
+++ client/src/app/pages/platform/pages/main-menu/main-menu.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 // PrimeNG
 import { ButtonModule } from 'primeng/button';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './main-menu.component.html'
 })
 export default class MainMenuComponent {}
