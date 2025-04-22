1a242fda6acf860a254a9a1796200dff6a112206
Kevin Collazos <kevcollazos@gmail.com>
2025-04-22 11:19:44 -0400

    ♻️ refactor(Sidebar): enhance sidebar functionality and styles, implement responsive behavior

--- client/src/app/shared/services/cache/cache.service.ts
+++ client/src/app/shared/services/cache/cache.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { computed, Injectable, signal, WritableSignal } from '@angular/core';
 import { DataCache } from '../../interfaces/cache.interface';
 import { GreenChecks } from '../../interfaces/get/get-green-checks.interface';
 
@@ -10,7 +10,16 @@ export class CacheService {
   isValidatingToken = signal(false);
   dataCache: WritableSignal<DataCache> = signal(localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data') ?? '') : {});
   windowHeight = signal(window.innerHeight);
+  windowWidth = signal(window.innerWidth);
   greenChecks = signal<GreenChecks>({});
   currentResultIsLoading = signal(false);
   currentResultId: WritableSignal<number> = signal(0);
+  hasSmallScreen = computed(() => this.windowHeight() < 768);
+  hasSmallScreenWidth = computed(() => this.windowWidth() < 1280);
+  isSidebarCollapsed = signal<boolean>(localStorage.getItem('isSidebarCollapsed') === 'true');
+
+  toggleSidebar() {
+    this.isSidebarCollapsed.update(isCollapsed => !isCollapsed);
+    localStorage.setItem('isSidebarCollapsed', this.isSidebarCollapsed().toString());
+  }
 }
