88f72ec2a99cc38e77e9110da3e523b59d30f611
Kevin Collazos <kevcollazos@gmail.com>
2025-04-23 13:33:35 -0400

    ✨ feat(Breadcrumb): refactor breadcrumb component to utilize PageTitleService for dynamic breadcrumb management and simplify route handling

--- client/src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ client/src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Breadcrumb } from 'primeng/breadcrumb';
-import { ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
-import { Subject, filter, takeUntil } from 'rxjs';
-
-interface BreadcrumbItem {
-  path: string;
-  label: string;
-}
+import { PageTitleService } from '../../services/page-title.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -17,53 +12,21 @@ interface BreadcrumbItem {
 })
 export class BreadcrumbComponent implements OnInit, OnDestroy {
   items: MenuItem[] = [];
-  home: MenuItem = { icon: 'pi pi-home', routerLink: '/' };
-
-  private activatedRoute = inject(ActivatedRoute);
-  private router = inject(Router);
   private destroy$ = new Subject<void>();
+  private pageTitleService = inject(PageTitleService);
+
+  get home(): MenuItem {
+    return this.pageTitleService.home;
+  }
 
   ngOnInit() {
-    // Detectar cambios de ruta
-    this.router.events
-      .pipe(
-        filter(event => event instanceof NavigationEnd),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(() => {
-        this.items = []; // Limpiar items antes de actualizar
-        this.updateBreadcrumb();
-      });
-
-    // Cargar breadcrumb inicial
-    this.updateBreadcrumb();
+    this.pageTitleService.breadcrumb$.pipe(takeUntil(this.destroy$)).subscribe(items => {
+      this.items = items;
+    });
   }
 
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
-  }
-
-  private updateBreadcrumb(): void {
-    const route: ActivatedRouteSnapshot | null = this.activatedRoute.snapshot;
-
-    // Buscar en los hijos de la ruta actual
-    if (route.children && route.children.length > 0) {
-      route.children.forEach(child => {
-        if (child.data && child.data['breadcrumb']) {
-          const breadcrumbData = child.data['breadcrumb'] as BreadcrumbItem[];
-          // Asignar nuevos items solo si hay datos de breadcrumb
-          if (breadcrumbData.length > 0) {
-            this.items = breadcrumbData.map(item => ({
-              label: item.label,
-              routerLink: `/${item.path}`
-            }));
-          }
-        }
-      });
-    }
-
-    console.log('Route:', route);
-    console.log('Items:', this.items);
   }
 }
