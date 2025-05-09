a7868038874f1f707ae236db8e1ce985b8e6a4d2
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-05-09 14:31:28 -0500

    :white_check_mark: test: test added

--- client/src/app/pages/platform/pages/unauthorized/unauthorized.component.spec.ts
+++ client/src/app/pages/platform/pages/unauthorized/unauthorized.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import UnauthorizedComponent from './unauthorized.component';
 
 describe('UnauthorizedComponent', () => {
@@ -8,7 +8,7 @@ describe('UnauthorizedComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UnauthorizedComponent]
+      imports: [UnauthorizedComponent, RouterTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UnauthorizedComponent);
