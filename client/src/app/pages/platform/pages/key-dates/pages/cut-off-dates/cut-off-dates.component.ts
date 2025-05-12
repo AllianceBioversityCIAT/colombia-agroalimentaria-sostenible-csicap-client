d01b87edc9ea0809b4a8b0e9e77d9af46a992620
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-05-12 16:02:49 -0500

    :sparkles: feat: pipes added

--- client/src/app/pages/platform/pages/key-dates/pages/cut-off-dates/cut-off-dates.component.ts
+++ client/src/app/pages/platform/pages/key-dates/pages/cut-off-dates/cut-off-dates.component.ts
@@ -2,11 +2,11 @@ import { Component, inject, signal, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { GetCutOffDates } from 'src/app/shared/interfaces/get/get-cut-off-dates.interface';
-import { DatePipe } from '@angular/common';
+import { DatePipe, CommonModule, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-cut-off-dates',
-  imports: [TableModule, DatePipe],
+  imports: [TableModule, DatePipe, CommonModule, TitleCasePipe],
   templateUrl: './cut-off-dates.component.html',
   styleUrl: './cut-off-dates.component.scss'
 })
@@ -25,7 +25,6 @@ export default class CutOffDatesComponent implements OnInit {
       console.log(res.data);
       res.data.forEach(date => {
         date.stateClass = date.estado === 'ABIERTO' ? 'bg-green-500' : date.estado === 'CERRADO' ? 'bg-red-500' : 'bg-orange-500';
-        date.estadoCase = date.estado.charAt(0).toUpperCase() + date.estado.slice(1).toLowerCase();
       });
       this.dates.set(res.data);
     });
