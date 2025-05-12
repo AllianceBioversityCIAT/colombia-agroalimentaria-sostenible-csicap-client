2cecf11802df26fa8cba92c54365a29fc5b3e847
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-05-12 17:53:07 -0500

    :sparkles: feat: structure added

--- client/src/app/pages/platform/pages/operational-plans/pages/see-byproducts/see-byproducts.component.ts
+++ client/src/app/pages/platform/pages/operational-plans/pages/see-byproducts/see-byproducts.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-see-byproducts',
-  imports: [],
-  templateUrl: './see-byproducts.component.html',
-  styleUrl: './see-byproducts.component.scss'
+  imports: [TabViewModule, ButtonModule, TooltipModule, CommonModule, RouterModule],
+  templateUrl: './see-byproducts.component.html'
 })
-export default class SeeByproductsComponent {}
+export default class SeeByproductsComponent {
+  subproductos = [
+    { id: 1, nombre: 'Subproducto 1', activo: true },
+    { id: 2, nombre: 'Subproducto 2', activo: false },
+    { id: 3, nombre: 'Subproducto 3', activo: false },
+    { id: 4, nombre: 'Subproducto 4', activo: false }
+  ];
+}
