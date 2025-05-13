7dcf984518b57008611fc783a3b737b80fe79225
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-05-13 08:56:41 -0500

    :white_check_mark: test: test added

--- client/src/app/pages/platform/pages/operational-plans/pages/see-byproducts/see-byproducts.component.spec.ts
+++ client/src/app/pages/platform/pages/operational-plans/pages/see-byproducts/see-byproducts.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import SeeByproductsComponent from './see-byproducts.component';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TabsModule } from 'primeng/tabs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
-import { SeeByproductsComponent } from './see-byproducts.component';
+// Mock para ResizeObserver utilizado por PrimeNG Tabs
+class MockResizeObserver {
+  observe = jest.fn();
+  unobserve = jest.fn();
+  disconnect = jest.fn();
+}
+
+// Configuración global del mock
+global.ResizeObserver = MockResizeObserver;
 
 describe('SeeByproductsComponent', () => {
   let component: SeeByproductsComponent;
@@ -8,9 +25,10 @@ describe('SeeByproductsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SeeByproductsComponent]
-    })
-    .compileComponents();
+      imports: [SeeByproductsComponent, ButtonModule, TooltipModule, CommonModule, RouterTestingModule, TabsModule],
+      providers: [provideNoopAnimations()],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SeeByproductsComponent);
     component = fixture.componentInstance;
