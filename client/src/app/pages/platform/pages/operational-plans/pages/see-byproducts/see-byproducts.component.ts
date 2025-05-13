0eec92381c695c132ee1b8314560659469d2f314
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-05-13 09:11:07 -0500

    :sparkles: feat: styles added

--- client/src/app/pages/platform/pages/operational-plans/pages/see-byproducts/see-byproducts.component.ts
+++ client/src/app/pages/platform/pages/operational-plans/pages/see-byproducts/see-byproducts.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TabsModule } from 'primeng/tabs';
-
+import { DialogModule } from 'primeng/dialog';
 @Component({
   selector: 'app-see-byproducts',
-  imports: [ButtonModule, TooltipModule, CommonModule, RouterModule, TabsModule],
+  imports: [ButtonModule, TooltipModule, CommonModule, RouterModule, TabsModule, DialogModule],
   templateUrl: './see-byproducts.component.html'
 })
 export default class SeeByproductsComponent {
+  dialogVisible = signal({ value: true });
   subproductos = [
     { id: 1, nombre: 'Subproducto 1', activo: true },
     { id: 2, nombre: 'Subproducto 2', activo: false },
